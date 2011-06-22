7a160d01d696b36a7315b61375063c77fd75d2b4
abhishekkr <abhishk@thoughtworks.com>
2011-06-22 21:17:37 +0530

    flat.py script created to Generate GAE Project ready to copy Static Content and Update to AppEngine

---
+++ gae-flat-web/main.py
@@ -0,0 +1,24 @@
+"""[ main.py ]
+it's just here to redirect '/' to '/index.htm'
+the redirect-link could be changed by changing the link below
+"""
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+class MainHandler(webapp.RequestHandler):
+  def get(self):
+    self.redirect('/flat_web/index.htm')
+  def post(self):
+    self.redirect('/flat_web/index.htm')
+
+def main():
+  application = webapp.WSGIApplication(
+    [
+	  ('/', MainHandler),
+	], debug=True)
+  util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+  main()
